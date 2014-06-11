58aba2832dad052ab0ed1e09d718cb5a07faf655
marothon <marothon@gmail.com>
2014-06-11 10:29:57 +0200

    part of previous commit

--- gmaps_ev_viz/src/se/kth/ev/gmapsviz/MainActivity.java
+++ gmaps_ev_viz/src/se/kth/ev/gmapsviz/MainActivity.java
@@ -3,42 +3,61 @@ package se.kth.ev.gmapsviz;
 import java.util.List;
 import se.kth.ev.gmapsviz.APIDataTypes.DirectionsResult;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.api.client.json.JsonParser;
 import com.google.maps.android.PolyUtil;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
+	GoogleMap gmap;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		String key = getString(R.string.google_browser_api_key);
 		GoogleAPIQueries.setKey(key);
-		
+
 		try {
-			String res = GoogleAPIQueries.requestDirections(
-					"Chicago,IL", "Los Angeles,CA").get();
-			
-			//DirectionsResult directionsResult = gson.fromJson(res, DirectionsResult.class);
-			JsonParser fact = APIRequest.JSON_FACTORY.createJsonParser(res);
-			DirectionsResult directionsResult = fact.parse(DirectionsResult.class);
+			String api_data = GoogleAPIQueries.requestDirections("Chicago,IL",
+					"Los Angeles,CA").get();
+
+			JsonParser fact = APIRequest.JSON_FACTORY
+					.createJsonParser(api_data);
+			DirectionsResult directionsResult = fact
+					.parse(DirectionsResult.class);
 			String encodedPoints = directionsResult.routes.get(0).overviewPolyLine.points;
 			List<LatLng> points = PolyUtil.decode(encodedPoints);
-			for(LatLng l : points){
-				Log.d("po", l.toString());
-			}
-			// Log.d("encodedPoints", encodedPoints);
+			MapFragment fm = (MapFragment) getFragmentManager()
+					.findFragmentById(R.id.map);
+
+			gmap = fm.getMap();
+			PolylineOptions line = new PolylineOptions();
+			line.addAll(points);
+			line.width(4);
+			line.color(Color.BLUE);
+			gmap.addPolyline(line);
+			gmap.moveCamera(CameraUpdateFactory.newLatLngZoom(points.get(0), 10.0f));
 			
+			//for (LatLng l : points) {
+			//	Log.d("po", l.toString());
+			//}
+			// Log.d("encodedPoints", encodedPoints);
+
 		} catch (Exception e) {
 			e.printStackTrace();
-		} 
-		//GoogleAPIQueries.requestElevation("39.7391536,-104.9847034");
+		}
+		// GoogleAPIQueries.requestElevation("39.7391536,-104.9847034");
 
 	}
 
