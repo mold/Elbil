6f313f3ba8592240ef10e8f5cf77838664e71f60
John Turesson <john.brynte@gmail.com>
2014-08-20 18:29:46 +0200

    f-f-f-fine driving!

--- project elvizp/src/com/kth/ev/differentiatedrange/gamification/FineDriver.java
+++ project elvizp/src/com/kth/ev/differentiatedrange/gamification/FineDriver.java
@@ -45,39 +45,56 @@ public class FineDriver {
 	// values below this value are considered bad
 	private final double NEGATIVE_LIMIT = -0.2;
 
-	// the interval of loosing points in NORMAL mode
+	// the interval of loosing points in the different modes
 	private final long NORMAL_POINTS_INTERVAL = 4000;
-	private final long GOOD_POINTS_INTERVAL = 1000;
-	private final long BAD_POINTS_INTERVAL = 1000;
+	private final long GOOD_POINTS_INTERVAL = 2000;
+	private final long BAD_POINTS_INTERVAL = 2000;
+	// a value to see how long time is spent in a mode
+	private final long MODE_OVERRIDE_TIME = 30000;
 
+	private long modeStartTimeStamp;
 	private long modeTimeStamp;
 	private long pointsTimeStamp;
 	private double consumptionDifference;
 	private double consumptionOverflow;
 	private Mode mode;
 	private Mode currentMode;
-	private int points;
 	private double transitionTime;
+	private double modeOverrideTime;
 
 	// View
 	TextView pointsText;
 	TextView consumptionText;
 	TextView modeText;
 	TextView debugText;
+	TextView levelText;
+	TextView scoringText;
 	Handler uiHandler;
 	Resources resources;
 	boolean viewInitialized;
+	
+	// scoring
+	private int points;
+	private int level;
+	private int prevLevelPoints;
+	private int nextLevelPoints;
+	private final double LOOSE_LEVEL_LIMIT = 0.9;
 
 	public FineDriver() {
 		uiHandler = new Handler();
 		viewInitialized = false;
+		reset();
 	}
 
 	public void reset() {
 		consumptionDifference = 0;
 		points = 0;
+		level = 0;
+		prevLevelPoints = 0;
+		nextLevelPoints = 50;
 		mode = Mode.NORMAL;
 		currentMode = Mode.NORMAL;
+		modeStartTimeStamp = System.currentTimeMillis();
 	}
 
 	public void setConsumptionDifference(double difference) {
@@ -95,6 +112,7 @@ public class FineDriver {
 			if (mode != Mode.GOOD) {
 				if (time - modeTimeStamp > ACTIVATION_TIME) {
 					mode = Mode.GOOD;
+					modeStartTimeStamp = time;
 					Log.v(TAG, "Switched to GOOD mode");
 					PdBase.sendBang(R_GOOD_BANG);
 					PdBase.sendBang(R_STATE_BANG);
@@ -115,6 +133,7 @@ public class FineDriver {
 			if (mode != Mode.BAD) {
 				if (time - modeTimeStamp > ACTIVATION_TIME) {
 					mode = Mode.BAD;
+					modeStartTimeStamp = time;
 					Log.v(TAG, "Switched to BAD mode");
 					PdBase.sendBang(R_BAD_BANG);
 					PdBase.sendBang(R_STATE_BANG);
@@ -145,6 +164,7 @@ public class FineDriver {
 				}
 				if (time - modeTimeStamp > DEACTIVATION_TIME) {
 					mode = Mode.NORMAL;
+					modeStartTimeStamp = time;
 					Log.v(TAG, "Switched to NORMAL mode");
 					PdBase.sendBang(R_NORMAL_BANG);
 					PdBase.sendBang(R_STATE_BANG);
@@ -156,17 +176,21 @@ public class FineDriver {
 		}
 
 		// calculate points
+		modeOverrideTime = (time - modeStartTimeStamp) / (double) MODE_OVERRIDE_TIME;
+		modeOverrideTime = modeOverrideTime > 1 ? 1 : modeOverrideTime;
 		switch (mode) {
 		case NORMAL:
-			if (time - pointsTimeStamp > NORMAL_POINTS_INTERVAL) {
+			if (time - pointsTimeStamp > NORMAL_POINTS_INTERVAL * (1 - modeOverrideTime * 0.5)) {
 				points -= 1;
+				managePoints();
 				Log.v(TAG, "points: " + points);
 				pointsTimeStamp = time;
 			}
 			break;
 		case GOOD:
-			if (time - pointsTimeStamp > GOOD_POINTS_INTERVAL) {
+			if (time - pointsTimeStamp > GOOD_POINTS_INTERVAL * (1 - modeOverrideTime * 0.8)) {
 				points += 1;
+				managePoints();
 				pointsTimeStamp = time;
 			}
 			consumptionOverflow = (float) (difference - POSITIVE_LIMIT);
@@ -175,8 +199,9 @@ public class FineDriver {
 			PdBase.sendFloat(R_CONSUMPTION, (float) consumptionOverflow);
 			break;
 		case BAD:
-			if (time - pointsTimeStamp > BAD_POINTS_INTERVAL) {
+			if (time - pointsTimeStamp > BAD_POINTS_INTERVAL * (1 - modeOverrideTime * 0.8)) {
 				points -= 1;
+				managePoints();
 				pointsTimeStamp = time;
 			}
 			consumptionOverflow = (float) (difference - NEGATIVE_LIMIT);
@@ -197,16 +222,33 @@ public class FineDriver {
 		consumptionText = (TextView) v.findViewById(R.id.consumption_text);
 		modeText = (TextView) v.findViewById(R.id.mode_text);
 		debugText = (TextView) v.findViewById(R.id.debug_text);
+		levelText = (TextView) v.findViewById(R.id.level_text);
+		scoringText = (TextView) v.findViewById(R.id.scoring_text);
 
 		viewInitialized = true;
 
 		updateView();
+	}
+	
+	private void managePoints() {
+		if (points > 0 && points <= prevLevelPoints * LOOSE_LEVEL_LIMIT) {
+			if (level > 0) {
+				level -= 1;
+				nextLevelPoints = prevLevelPoints;
+				prevLevelPoints /= 2;
+			}
+		} else if (points >= nextLevelPoints) {
+			level += 1;
+			prevLevelPoints = nextLevelPoints;
+			nextLevelPoints *= 2;
+		}
 	}
 
 	private class UIThread implements Runnable {
 
 		@Override
 		public void run() {
+			levelText.setText("Level: " + level);
 			pointsText.setText("Points: " + points);
 			consumptionText.setText("Consumption overflow: "
 					+ Math.round(consumptionOverflow * 1000) / 1000.0
@@ -224,7 +266,8 @@ public class FineDriver {
 				modeText.setTextColor(resources.getColor(R.color.red));
 				break;
 			}
-			debugText.setText("Transition time: " + Math.round(1000*transitionTime) / 1000.0);
+			debugText.setText("Transition time: " + Math.round(1000*transitionTime) / 1000.0 + ", Mode override time: " + modeOverrideTime);
+			scoringText.setText("Next level: " + nextLevelPoints + " (previous level: " + Math.round(prevLevelPoints * LOOSE_LEVEL_LIMIT) + ")");
 		}
 
 	}
